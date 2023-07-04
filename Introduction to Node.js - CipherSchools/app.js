fd7ac396111729e7d640163706214f2b2438920d
Mohammad Dilshad <90137093+mohammaddilshad@users.noreply.github.com>
2023-07-04 23:24:23 +0530

    Update app.js

--- Introduction to Node.js - CipherSchools/app.js
+++ Introduction to Node.js - CipherSchools/app.js
@@ -1,1 +1,23 @@
-console.log(global);
+// console.log(global);
+
+// setTimeout( () => {
+//     console.log('Timed it for 2 seconds')
+// }, 2000)
+
+// setInterval( () => {
+//     console.log('Timed interval for 2 seconds')
+// }, 2000)
+
+// let time = 0;
+// let timer = setInterval( () => {
+//     time += 2;
+//     console.log(time + ' seconds have passed');
+//     if (time > 10) {
+//         clearInterval(timer);
+//     }
+// }, 2000)
+
+
+// utility function
+console.log(__dirname);
+console.log(__filename);
